b2dc30082d08754f9ed219c5a0c33206e6f0a4fe
Chu Thế Văn <chuthevan450@gmail.com>
2026-01-20 09:21:53 +0700

    Fix fibonacci sequence calculation and output

--- project/J1.S.P0009/src/Main.java
+++ project/J1.S.P0009/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     // Chỉ có những thằng static thì mới chơi với nhau, ko cần phải tạo đối tượng thì vẫn gọi được static :))))
     public static int fibonacciHelper(int term, int lower, int higher) {
 
-        // in ra 2 giá trị đầu tiên
-        if (term < 2) {
+        // khi term đã đến 0, chương trình sẽ dừng ở higher
+        if (term <= 0) {
             return higher;
         }
         System.out.print(higher + ", ");
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("===== [LAB211] J1.S.P0009 - FIBONACCI =====");
         System.out.println("The 45 sequence fibonacci: ");
-        fibonacciHelper(46, 1, 0);
+        fibonacciHelper(45, 1, 0);
         System.out.println("\n===== END =====");
     }
 }
